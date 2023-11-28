7d45850ee40db83845852b2e5170dbe141f6055e
CodingRabbit <152000818+C0d1ngR4bb1t@users.noreply.github.com>
2023-11-28 17:19:05 +0100

    Update alerts1.js

--- assets/js/alerts1.js
+++ assets/js/alerts1.js
@@ -3,6 +3,7 @@
     alert("Welcome to C0d1ngR4bb1t!");
     alert("Here you will find my projects, some javascript bookmarklets...\nHope you like them!");
     alert("Remember to star my projects if you like them :P");
+    localStorage["remember_alerts"] = 1;
   };
   else if (localStorage.get("remember_alerts") == 1) {
     return;
