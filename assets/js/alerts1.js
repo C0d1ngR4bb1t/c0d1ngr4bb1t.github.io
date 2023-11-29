4bac7c89cf6a4b981d551f1d90ba667fe9de246f
CodingRabbit <152000818+C0d1ngR4bb1t@users.noreply.github.com>
2023-11-29 17:24:47 +0100

    Update alerts1.js

--- assets/js/alerts1.js
+++ assets/js/alerts1.js
@@ -1,12 +1,12 @@
-window.localStorage.setItem('remember_alerts', 0);
-if (window.localStorage.getItem("remember_alerts") == 0) {
+window.localStorage.setItem('remember_alerts', '0');
+if (window.localStorage.getItem("remember_alerts") == '0') {
     alert("Welcome to C0d1ngR4bb1t!");
     alert("Here you will find my projects, some javascript bookmarklets...\nHope you like them!");
     alert("Remember to star my projects if you like them :P");
-    window.localStorage.setItem("remember_alerts", 1);
-} else if (window.localStorage.getItem("remember_alerts") == 1) {
+    window.localStorage.setItem("remember_alerts", "1");
+}; else if (window.localStorage.getItem("remember_alerts") == "1") {
     return;
-} else {
-    console.log("Invalid LocalStorage Value. Resetting...");
-    window.localStorage.setItem('remember_alerts', 0);
+}; else {
+//    console.log("Invalid LocalStorage Value. Resetting...");
+    window.localStorage.setItem('remember_alerts', '0');
 }
