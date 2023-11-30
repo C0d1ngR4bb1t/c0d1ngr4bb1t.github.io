48881ddf7b35e3645f35d2596d5aa522d8532b88
CodingRabbit <152000818+C0d1ngR4bb1t@users.noreply.github.com>
2023-11-30 14:26:30 +0100

    Update alerts1.js

--- assets/js/alerts1.js
+++ assets/js/alerts1.js
@@ -1,10 +1,10 @@
 // testing purposes:
 // window.localStorage.setItem('remember_alerts', '0');
-if (window.localStorage.getItem("remember_alerts") == '0') {
+if (window.localStorage.getItem("remember_alerts") == 0) {
     alert("Welcome to C0d1ngR4bb1t!");
     alert("Here you will find my projects, some javascript bookmarklets...\nHope you like them!");
     alert("Remember to star my projects if you like them :P");
-    window.localStorage.setItem("remember_alerts", "1");
+    window.localStorage.setItem("remember_alerts", 1);
 } else {
-    console.log("Not showing up alerts.");
+    console.log("Skipping alerts...");
 }
